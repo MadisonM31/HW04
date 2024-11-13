2d0840f8a8ee7d9c942e5fc8f61fdfcc736b9466
Madison Manning <mmannin3@uwyo.edu>
2024-11-13 05:31:39 +0000

    finalized

---
+++ ManningMadison-HW04.py
@@ -0,0 +1,33 @@
+# Madison Manning
+# UWYO COSC 1010
+# 11/12/2024
+# HW 04
+# Lab Section: 10
+# Sources, people worked with, help given to: 
+
+from pathlib import Path 
+imp_path = Path("prompt.txt")
+imp_prompt = imp_path.read_text()
+lines = imp_prompt.splitlines()
+
+orders = []
+config = ""
+
+for line in lines:
+    orders = line.split('\t')
+    for order in orders:
+        num = order[2:]
+        if len(order) > 0:
+            if order[0] == "w":
+                config += " " * int(num)
+            else:
+                config += "*" * int(num)
+    config += "\n"
+
+out_path = Path("out.txt")
+out_path.write_text(config)
+
+
+
+
+
